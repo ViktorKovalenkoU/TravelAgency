af82c911976410131c464f01237dd821d53f7ffb
ViktorKovalenkoU <132162098+ViktorKovalenkoU@users.noreply.github.com>
2025-06-22 16:22:43 +0300

    Admin functionality added (Manager`s copy)

---
+++ src/main/java/com/epam/finaltask/controller/AdminController.java
@@ -0,0 +1,48 @@
+package com.epam.finaltask.controller;
+
+import com.epam.finaltask.model.VoucherStatus;
+import com.epam.finaltask.service.VoucherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+@PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final VoucherService voucherService;
+
+    @GetMapping
+    public String panel(
+            @RequestParam(value = "lang", defaultValue = "en") String lang,
+            Model model) {
+
+        model.addAttribute("vouchers", voucherService.findAll(lang));
+        model.addAttribute("allStatuses", VoucherStatus.values());
+        return "admin";
+    }
+
+    @PostMapping("/vouchers/{id}/hot")
+    public String toggleHot(
+            @PathVariable String id,
+            @RequestParam boolean hot,
+            @RequestParam(value = "lang", defaultValue = "en") String lang) {
+
+        voucherService.changeHotStatus(id, hot, lang);
+        return "redirect:/admin?lang=" + lang;
+    }
+
+    @PostMapping("/vouchers/{id}/status")
+    public String changeStatus(
+            @PathVariable String id,
+            @RequestParam("status") VoucherStatus status,
+            @RequestParam(value = "lang", defaultValue = "en") String lang) {
+
+        voucherService.changeStatus(id, status, lang);
+        return "redirect:/admin?lang=" + lang;
+    }
+}
